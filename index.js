717ed52d3c0bdef287325974edc81a645bbcb389
James Yeates <jamesyeates@jamess-mbp.lan>
2022-10-13 09:24:27 -0600

    counter UI setup to state

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 // fontSize 16   number
 // backgroundColor string
 
+// variable I can use anywhere in this file
 let state = {
-    counter: 0,
+    counter: 4,
     fontSize: 16,
     backgroundColor: 'red'
 }
@@ -34,6 +35,33 @@ function decreaseFontSize(){
 function changeBackground(color){
    state.backgroundColor = color
 }
+
+// want to take my state and => html make it look nice, add buttons
+// forms so user can use it
+console.log('Working')
+
+//  visible to all of this file
+const root = document.querySelector('#root')
+
+// this function does the html stuff for
+// the counter feature
+function renderCounter(){
+   const counter = document.createElement('div')
+   counter.innerHTML=`
+      <div>
+          <h1>Counter ${state.counter}</h1>
+      </div>    
+   `
+   root.append(counter)
+}
+
+// this functions renders the elements to the dom
+function render(){
+   renderCounter()
+}
+
+// need to call!!!
+render()
 
 
 
